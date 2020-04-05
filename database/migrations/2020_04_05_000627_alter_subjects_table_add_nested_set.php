6a61331037f62007a16828bfc280b09b08b3fb27
Adam Botsford <abotsford@msdist.com>
2020-04-04 19:10:17 -0500

    Altered Subjects table, created fields for NestedSet

---
+++ database/migrations/2020_04_05_000627_alter_subjects_table_add_nested_set.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Kalnoy\Nestedset\NestedSet;
+
+class AlterSubjectsTableAddNestedSet extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('subjects', function (Blueprint $table) {
+            NestedSet::columns($table);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
